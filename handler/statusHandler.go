4b5879c1cbad54813343686f278794cf20c34aa8
vakvaer <vakvaer@ntnu.no>
2024-03-20 17:13:48 +0100

    adding status codes for APIs

--- handler/statusHandler.go
+++ handler/statusHandler.go
@@ -3,22 +3,47 @@ package handler
 import (
 	"assignment2/utils"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 )
 
-var startTime time.Time = time.Now()
+var startTime = time.Now()
 
 func StatusHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Time the server has been running since start
 	upTime := time.Since(startTime).Seconds()
 
+	// Gets status-code for Countries API
+	countriesApiStatusCode, err := http.Get(utils.COUNTRIES_API + "/name/norway")
+	if err != nil {
+		err = fmt.Errorf("error occured while making HTTP request: %v", err)
+		fmt.Println(err)
+		return
+	}
+
+	// Gets status-code for Meteo API (can be changed to its const)
+	meteoApiStatusCode, err := http.Get("https://geocoding-api.open-meteo.com/v1/search?name=Norway&count=1&language=en&format=json")
+	if err != nil {
+		err = fmt.Errorf("error occured while making HTTP request: %v", err)
+		fmt.Println(err)
+		return
+	}
+
+	// Gets status-code for Currency API
+	currencyApiStatusCode, err := http.Get(utils.CURRENCY_API + "/nok")
+	if err != nil {
+		err = fmt.Errorf("error occured while making HTTP request: %v", err)
+		fmt.Println(err)
+		return
+	}
+
 	// Creates status struct (hardcoded now)
 	Status := utils.Status{
-		Countriesapi:   1,
-		Meteoapi:       1,
-		Currencyapi:    1,
+		Countriesapi:   countriesApiStatusCode.StatusCode,
+		Meteoapi:       meteoApiStatusCode.StatusCode,
+		Currencyapi:    currencyApiStatusCode.StatusCode,
 		Notificationdb: 1,
 		Webhooks:       1,
 		Version:        "v1",
