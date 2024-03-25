3d0a936c301ea4c4cc70355159785148504dc5bb
kind2511 <chrisa2511@gmail.com>
2024-03-25 17:26:50 +0100

    added firebase and firestore setup in main.go

--- main.go
+++ main.go
@@ -3,14 +3,52 @@ package main
 import (
 	"assignment2/handler"
 	"assignment2/utils"
-	"fmt"
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
 )
 
+// Firebase context and client used by Firestore functions throughout the program.
+var ctx context.Context
+var client *firestore.Client
+
+// name of collection used for dashboard
+//const collection = "Dashboard"
+
+
 func main() {
-	fmt.Println("Hello World!")
+
+	// Firebase initialisation
+	ctx = context.Background()
+
+	// Loads credential file from firebase
+	sa := option.WithCredentialsFile("./prog2005-assignment2-ee93a-firebase-adminsdk-9o3qm-43d9d2d766.json")
+	app, err := firebase.NewApp(ctx, nil, sa)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// Instantiate client
+	client, err = app.Firestore(ctx)
+
+	// Check whether there is an error when connecting to Firestore
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// Close down client at the end of the function
+	defer func() {
+		errClose := client.Close()
+		if errClose != nil {
+			log.Fatal("Closing of the Firebase client failed. Error:", errClose)
+		}
+	}()
 
 	port := os.Getenv("PORT")
 
