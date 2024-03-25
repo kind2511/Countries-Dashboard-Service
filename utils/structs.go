15e5424e258e7808f937cc66c94b09abf0b08874
kind2511 <chrisa2511@gmail.com>
2024-03-25 16:51:59 +0100

    added structs for registration endpoints

--- utils/structs.go
+++ utils/structs.go
@@ -1,2 +1,20 @@
 package utils
 
+// Struct for registration
+type Registration struct {
+	Country  string   `json:"country"`
+	Isocode  string   `json:"isocode"`
+	Features Features `json:"features"`
+}
+
+// Struct for features
+type Features struct {
+	Temperature      bool     `json:"temperature"` // Note: In degrees Celsius
+	Precipitation    bool     `json:"precipitation"`
+	Capital          bool     `json:"capital"`
+	Coordinates      bool     `json:"coordinates"`
+	Population       bool     `json:"population"`
+	Area             bool     `json:"area"`
+	TargetCurrencies []string `json:"targetcurrencies"`
+}
+
